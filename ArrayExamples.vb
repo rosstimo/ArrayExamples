e0e09d76401fb3daff1cf72319aaf695b50fafbd
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2020-09-09 08:24:27 -0600

    Add project files.

---
+++ ArrayExamples.vb
@@ -0,0 +1,19 @@
+﻿Module ArrayExamples
+
+    Sub Main()
+        SimpleArray()
+    End Sub
+
+    Sub SimpleArray()
+        Dim names(5) As String
+
+        names(0) = "Bob"
+        names(3) = "Jimmy"
+
+        Console.WriteLine("Hello " & names(3))
+        Console.Read()
+
+    End Sub
+
+
+End Module
